e36c3722feb7812ee19673a4528bf753244204d4
sriram <sriram3720@gmail.com>
2022-09-18 00:07:09 +0530

    edited portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,8 +2,8 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'sriram_portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
@@ -11,16 +11,16 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Sriraman',
-  subtitle: '',
+  subtitle: 'I am a web Developer.',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'I am a fullstack web developer currently working on MERN stack.',
+  paragraphTwo: 'In frontend i am familiar in React,express framework and backend i know Nodejs. i am also good at sql and magoDb in database',
+  paragraphThree: 'I am self taught web developer .',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -29,11 +29,11 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Appletv_clone',
+    info: 'It is a clone of appletv streaming website.this website has many shows,webseries,sports live streaming.Appletv website is quiet good at UI design . It is built by using simple HTML ,CSS,Js without any framework.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'hhttps://user-images.githubusercontent.com/101914524/174465411-bffb31e9-32c1-472b-a049-562181b972c3.png', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
